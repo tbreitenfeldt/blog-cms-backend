d6336f61f9cc85df3d0a2ead01bd0f6698613250
tbreitenfeldt <timothyjb310@gmail.com>
2020-12-01 16:26:30 -0500

    Initialized project.

---
+++ src/main/java/com/timothybreitenfeldt/blog/BlogCmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.timothybreitenfeldt.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCmsBackendApplication.class, args);
+	}
+
+}
