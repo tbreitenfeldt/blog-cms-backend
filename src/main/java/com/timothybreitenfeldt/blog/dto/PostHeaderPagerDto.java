db4f4dce25c2d5a844ebde351a1fee48d561204c
tbreitenfeldt <timothyjb310@gmail.com>
2020-12-21 14:22:38 -0500

    Modified pagination object for posts DTO to change currentPage to pageNumber to be more consistent.

--- src/main/java/com/timothybreitenfeldt/blog/dto/PostHeaderPagerDto.java
+++ src/main/java/com/timothybreitenfeldt/blog/dto/PostHeaderPagerDto.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PostHeaderPagerDto {
 
-    private int currentPage;
+    private int pageNumber;
     private long pageSize;
     private int totalPages;
     private List<PostHeaderResponseDto> posts;
