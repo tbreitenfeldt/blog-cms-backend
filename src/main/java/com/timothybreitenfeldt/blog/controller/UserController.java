b61c4edbfe90bb6c1607ec86c00fac01e026ccc3
tbreitenfeldt <timothyjb310@gmail.com>
2020-12-23 14:32:09 -0500

    Added CrossOrigin annotation to PostController and UserController to allow all requests for CORS.

--- src/main/java/com/timothybreitenfeldt/blog/controller/UserController.java
+++ src/main/java/com/timothybreitenfeldt/blog/controller/UserController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.timothybreitenfeldt.blog.service.UserService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
